b59f35e8cdf8fa3c2a085b8210139f2356f25d0f
mithu0621 <mithunisk03@gmail.com>
2024-11-12 21:36:14 +0530

    first commit

---
+++ src/app/pages/manage-appointment/manage-appointment.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-appointment',
+  standalone: true,
+  imports: [],
+  templateUrl: './manage-appointment.component.html',
+  styleUrl: './manage-appointment.component.css'
+})
+export class ManageAppointmentComponent {
+
+}
